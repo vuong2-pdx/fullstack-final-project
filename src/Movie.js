08ec3e18b347489da2992225c2cc43acc3a0498a
Viktoriya <petrovav971@gmail.com>
2022-11-11 16:32:59 -0800

    Created movie module

---
+++ src/Movie.js
@@ -0,0 +1,14 @@
+//Movie Schema exported as a module
+const mongoose = require("mongoose");
+
+const movieSchema = mongoose.Schema({
+  movieID: { type: Number, required: [true, "Please add a movie ID"] },
+  poster: String,
+  title: String,
+  rating: Number,
+  review: String,
+  watched: Boolean,
+  //sources: [{name: String, url: String}], ???
+});
+
+module.exports = mongoose.model("Movie", movieSchema);
