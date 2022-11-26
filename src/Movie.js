8ae8229638356cfcb1204ffdd5edb51daecddbbc
Viktoriya <petrovav971@gmail.com>
2022-11-25 20:01:58 -0800

    Updated Movie schema

--- src/Movie.js
+++ src/Movie.js
@@ -5,12 +5,16 @@ const mongoose = require("mongoose");
 
 const movieSchema = mongoose.Schema({
   movieID: { type: Number, required: [true, "Please add a movie ID"] },
+  imbdID: Number,
   title: String,
+  type: String,
+  year: Number,
   poster: String,
   rating: Number,
+  plot: String,
   review: String,
   watched: Boolean,
-  //sources: [{name: String, url: String}], ???
+  sources: Array,
 });
 
 module.exports = mongoose.model("Movie", movieSchema);
